646f6728d32abec0dca699f6c11d3443d86a2ba6
Nenu-del <swethamudiam45@gmail.com>
2026-06-02 18:13:44 +0530

    files.py

---
+++ files.py
@@ -0,0 +1,4 @@
+Git commands are very important
+I am learning git commands
+I am ver new to the devops
+Updating the file..
---
+++ files.sh
@@ -0,0 +1,4 @@
+Hello
+HI
+Commandd
+We are discussing about the commands
